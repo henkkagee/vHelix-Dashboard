b28419b728d0d2bed8168c13e0208dcf63ab2242
Henrik Granö <henrik.grano@aalto.fi>
2020-10-29 20:30:25 +0200

    Add detailed Linux build instructions

--- src/build2/scaffold_routing_rectification/CMakeFiles/PXPHYSICS.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libPXPHYSICS.a"
-  "libPXPHYSICS.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/PXPHYSICS.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
